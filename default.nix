4beb2946aefafb519cb29fdcd11f87073479b05f
Michael Englehorn <michael@englehorn.com>
2025-10-12 05:42:04 -0500

    Updated deps

--- default.nix
+++ default.nix
@@ -1,7 +1,9 @@
 { pkgs ? import <nixpkgs> { } }:
+let manifest = (pkgs.lib.importTOML ./Cargo.toml).package;
+in
 pkgs.rustPlatform.buildRustPackage rec {
-  pname = "libfcc-rust";
-  version = "0.1.0";
+  pname = manifest.name;
+  version = manifest.version;
   cargoLock.lockFile = ./Cargo.lock;
   src = pkgs.lib.cleanSource ./.;
   nativeBuildInputs = [ pkgs.pkg-config ];
