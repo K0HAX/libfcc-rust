39660d88fa74b868084503042167396105a3880b
Michael Englehorn <michael@englehorn.com>
2025-10-12 05:59:11 -0500

    updated shell.nix

--- shell.nix
+++ shell.nix
@@ -10,18 +10,3 @@ pkgs.mkShell {
     ];
 }
 
-#let
-  #pkgs = import <nixpkgs> { config.allowUnfree = true; };
-#in
-#pkgs.mkShellNoCC {
-  #PKG_CONFIG_PATH = "${pkgs.openssl.dev}/lib/pkgconfig";
-  #packages = with pkgs; [
-    #gcc
-    #cargo
-    #rustfmt
-    #sqlite
-    #pkg-config
-  #];
-#}
-
-
