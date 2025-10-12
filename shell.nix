0300b124a4da13d1ea14f14dcba416c23da74f75
Michael Englehorn <michael@englehorn.com>
2025-10-12 05:55:34 -0500

    Testing an update of shell.nix

--- shell.nix
+++ shell.nix
@@ -1,15 +1,27 @@
-let
-  pkgs = import <nixpkgs> { config.allowUnfree = true; };
-in
-pkgs.mkShellNoCC {
-  PKG_CONFIG_PATH = "${pkgs.openssl.dev}/lib/pkgconfig";
-  packages = with pkgs; [
-    gcc
-    cargo
-    rustfmt
-    sqlite
-    pkg-config
-  ];
+{ pkgs ? import <nixpkgs> { } }:
+pkgs.mkShell {
+    # Get dependencies from the main package
+    inputsFrom = [ (pkgs.callPackage ./default.nix { }) ];
+    # Additional tooling
+    buildInputs = with pkgs; [
+        rust-analyzer   # LSP Server
+        rustfmt         # Formatter
+        clippy          # Linter
+    ];
 }
+
+#let
+  #pkgs = import <nixpkgs> { config.allowUnfree = true; };
+#in
+#pkgs.mkShellNoCC {
+  #PKG_CONFIG_PATH = "${pkgs.openssl.dev}/lib/pkgconfig";
+  #packages = with pkgs; [
+    #gcc
+    #cargo
+    #rustfmt
+    #sqlite
+    #pkg-config
+  #];
+#}
 
 
